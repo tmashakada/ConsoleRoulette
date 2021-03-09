73cc6ea7168999b4519376f625dc6ecb2f21f98c
tafadzwa mashakada <tmashakada10@gmail.com>
2021-03-09 14:09:00 +0200

    Edit main method

--- src/com/wonderlabz/consoleroulette/ConsoleRoulette.java
+++ src/com/wonderlabz/consoleroulette/ConsoleRoulette.java
@@ -5,6 +5,11 @@
  */
 package com.wonderlabz.consoleroulette;
 
+import com.wonderlabz.consoleroulette.model.GameProcessor;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author tmashakada email:tmashakada10@gmail.com
@@ -15,7 +20,17 @@ public class ConsoleRoulette {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            String playerlist="playerlist.txt";
+            String previousresults="previousresults.txt";
+            System.out.println("Running: " + new java.util.Date());
+            GameProcessor.processGameResults(playerlist, previousresults);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ConsoleRoulette.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
